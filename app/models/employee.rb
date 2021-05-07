b108f431b0b27ff4bfa8ca2b3de421740775e3bc
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-07 12:30:49 +0530

    Updated faker gem & updated employees table

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,28 @@
 class Employee < ApplicationRecord
 	has_many :forms
+
+	after_initialize do |user|
+	   puts "You have initialized an object!"
+	end
+
+	after_find do |user|
+	   puts "You have found an object!"
+	end
+
+	after_create :create_the_object
+	after_save :saving_the_object
+	after_commit :commit_the_object
+
+
+	def create_the_object
+		puts "creating of object"
+	end
+
+	def saving_the_object
+		puts "saving the object"
+	end
+
+	def commit_the_object
+		puts "commit the object"
+	end
 end
